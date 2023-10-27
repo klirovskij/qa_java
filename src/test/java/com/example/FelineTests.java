887cbac59c0c848a3523c9b4680688b8edce54a0
klirovskij <135542945+klirovskij@users.noreply.github.com>
2023-10-27 16:40:34 +0300

    Update FelineTests.java

--- src/test/java/com/example/FelineTests.java
+++ src/test/java/com/example/FelineTests.java
@@ -1,46 +1,71 @@
-package com.example;
-
-import org.hamcrest.MatcherAssert;
+import com.example.Animal;
+import com.example.Feline;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.Arrays;
+import java.util.List;
 
-import static org.hamcrest.CoreMatchers.equalTo;
+@RunWith(MockitoJUnitRunner.class)
+public class FelineTest {
 
-public class FelineTests {
-
-    @Test
-    public void getFamilyIsCorrect() {
-        String expectedFelineFamilyName = "Кошачьи";
-        MatcherAssert.assertThat("Некорректное название семейства кошачьих",
-                new Feline().getFamily(),
-                equalTo(expectedFelineFamilyName)
-        );
-    }
-
-    @Test
-    public void getKittensInputCountIsCorrect() {
-        int expectedCount = 5;
-        MatcherAssert.assertThat("Некорректное количество котят",
-                new Feline().getKittens(expectedCount),
-                equalTo(expectedCount)
-        );
-    }
+    @Mock
+    Animal animal;
 
     @Test
-    public void getKittensDefaultIsCorrect() {
-        int expectedCount = 1;
-        MatcherAssert.assertThat("Некорректное количество котят",
-                new Feline().getKittens(),
-                equalTo(expectedCount)
-        );
-    }
-
-    @Test
-    public void eatMeatIsCorrect() throws Exception {
+    public void testEatMeat() throws Exception {
+        //Arrange
         Feline feline = new Feline();
+        Mockito.when(animal.getFood("Хищник")).thenReturn(Arrays.asList("Животные", "Птицы", "Рыба"));
+        List<String> expected = animal.getFood("Хищник");
+       
+        //Act
+        List<String> actual = feline.eatMeat();
 
-        MatcherAssert.assertThat("Неправильный список еды",
-                feline.eatMeat(),
-                equalTo(feline.getFood("Хищник"))
-        );
+        //Assert
+        Assert.assertEquals("Meat не соответствует", expected, actual);
     }
+
+    @Test
+    public void testGetFamily(){
+        //Arrange
+        Feline feline = new Feline();
+        String expected = "Кошачьи";
+
+        //Act
+        String actual = feline.getFamily();
+
+        //Assert
+        Assert.assertEquals("Family не соответствует",expected, actual);
+    }
+
+    @Test
+    public void testGetKittensNull(){
+        //Arrange
+        Feline feline = new Feline();
+        int expected = 1;
+
+        //Act
+        int actual = feline.getKittens();
+
+        //Assert
+        Assert.assertEquals("kittensCount не соответствует", expected, actual);
+    }
+
+    @Test
+    public void testGetKittensNotNull(){
+        //Arrange
+        Feline feline = new Feline();
+        int expected = 1;
+
+        //Act
+        int actual = feline.getKittens(1);
+
+        //Assert
+        Assert.assertEquals("kittensCount не соответствует", expected, actual);
+    }
+
 }
