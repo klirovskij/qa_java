12d1f8c54f133fe267c4800f58e88730d540edc7
klirovskij <135542945+klirovskij@users.noreply.github.com>
2023-10-27 16:34:20 +0300

    Update AnimalTests.java

--- src/test/java/com/example/AnimalTests.java
+++ src/test/java/com/example/AnimalTests.java
@@ -1,11 +1,11 @@
 package com.example;
 
-import org.hamcrest.MatcherAssert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class AnimalTests {
 
@@ -16,17 +16,21 @@ public class AnimalTests {
     public void getFamilyIsCorrect() {
         String expectedString = "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи";
 
-        MatcherAssert.assertThat("Некорректный перечень семейств",
+        assertThat("Некорректный перечень семейств",
                 new Animal().getFamily(),
                 equalTo(expectedString)
         );
     }
 
     @Test
-    public void getFoodThrowsException() throws Exception {
-        expectedEx.expect(Exception.class);
-        expectedEx.expectMessage("Неизвестный вид животного, используйте значение Травоядное или Хищник");
-
-        new Animal().getFood("");
+    public void getFoodThrowsException() {
+        try {
+            new Animal().getFood("");
+        } catch (Exception e) {
+            assertThat("Неизвестный вид животного, используйте значение Травоядное или Хищник",
+                    e.getMessage(),
+                    equalTo("Неизвестный вид животного, используйте значение Травоядное или Хищник")
+            );
+        }
     }
 }
